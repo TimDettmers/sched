0bc7729091d235b26fa308b3dd1b6301cc982c7d
T. Dettmers <dettmers@mox2.hyak.local>
2019-10-16 09:31:56 -0700

    Added support for action_true args.

--- scripts/convtransformer/2D.py
+++ scripts/convtransformer/2D.py
@@ -16,7 +16,7 @@ s.update_host_config('home', mem_threshold=1700, util_threshold=30)
 s.update_host_config('office', mem_threshold=1700, util_threshold=25)
 #s.update_host_config('ari', mem_threshold=2500, util_threshold=25)
 
-cmd_raw = 'OMP_NUM_THREADS=1 python train.py --cuda --data ../data/wikitext-2/ --dataset wt103 --adaptive --n_layer 12 --dropatt 0.0 --optim adam --warmup_step 800 --tgt_len 150 --mem_len 150 --eval_tgt_len 150 --batch_size 32 --batch_chunk 1 --fp16 --dynamic-loss-scale --eval-interval 100 --work_dir=LM-TFM-wt103/SEED/ --log-interval 10'
+cmd_raw = 'OMP_NUM_THREADS=1 python train.py --cuda --data ../data/wikitext-2/ --dataset wt103 --adaptive --n_layer 12 --dropatt 0.0 --optim adam --tgt_len 150 --mem_len 150 --eval_tgt_len 150 --batch_size 32 --batch_chunk 1 --fp16 --dynamic-loss-scale --eval-interval 100 --work_dir=LM-TFM-wt103/ITER/ --log-interval 10'
 
 emb = 400
 model = 400
@@ -31,8 +31,8 @@ cmd = cmd_raw.format(emb, model, heads, d_head, inner, dropout, lr)
 
 args2 = {}
 args2['conv'] = ''
-#args2['dim2'] = ''
-#args2['shape2'] = 1
+args2['dim2'] = ''
+args2['shape2'] = 1
 args2['kernel-size'] = 1
 #args2['downsample-identity'] = ''
 args2['d_emb'] = 400
@@ -41,43 +41,48 @@ args2['n_head'] = 10
 args2['d_head'] = 40
 args2['d_inner'] = 2000
 args2['dropout'] = 0.1
-args2['lr'] = 0.0006
-args2['max_step'] = 4000
+args2['lr'] = 0.0008
+args2['max_step'] = 2000
+args2['warmup_step'] = 100
 
 log_base = '/usr/lusers/dettmers/logs/'
-logfolder = 'conv1d'
+logfolder = 'convtransformers/{0}/'.format('base2d')
 time_hours = 1
+
+cores_per_job = 2
 
 for key, value in args2.items():
     cmd = cmd + ' --{0} {1}'.format(key, value)
 
 args3 = {}
-#args3['max_step'] = [4000, 6000]
-#args3['lr'] = [0.0003, 0.0006]
+args3[''] = ['downsample-identity', '']
 
 args_prod = []
 for key, values in args3.items():
-    keyvalues = [' --{0} {1}'.format(key, v) for v in values]
+    if len(key) == 0:
+        keyvalues = [' --{0}'.format(v) if len(v) > 0 else '{0}'.format(v) for v in values]
+    else:
+        keyvalues = [' --{0} {1}'.format(key, v) for v in values]
     args_prod.append(keyvalues)
 
 if len(args_prod) >= 2:
     args_prod = list(product(*args_prod))
+else:
+    args_prod = [[args_prod[0][0]], [args_prod[0][1]]]
 
-num_seeds = 4
+num_seeds = 2
 seed_offset = 0
 
 jobs = []
 for seed in range(num_seeds):
     if len(args_prod) == 0: args_prod.append(('', ''))
-    for i, (key, value) in enumerate(args_prod):
+    for i, values in enumerate(args_prod):
         fp16 = False
-        jobs.append(['convtransformers/{0}/'.format(logfolder), 'convtransformer/pytorch/', cmd.replace('SEED', str(i)) + ' {1} {2} --seed {0}'.format(seed, key, value), time_hours, fp16])
-
-print(jobs[0])
-print(len(jobs))
-
-for job in jobs:
-    s.add_job(*job)
+        job_cmd = cmd.replace('ITER', str(i)) + ' --seed {0}'.format(seed)
+        for val in values:
+            job_cmd += ' {0}' .format(val)
+        jobs.append(job_cmd)
+        s.add_job(logfolder, 'convtransformer/pytorch/', job_cmd, time_hours, fp16, cores=cores_per_job)
 
 host2cmd = {}
 host2cmd['ofir3'] = ' --max-threads 4'
@@ -102,9 +107,9 @@ remap[('shoob', 0)] = 2
 
 if args.dry:
     for job in jobs:
-        print(job[2])
+        print(job)
     print('total jobs', len(jobs))
-    print('Jobs will be written to: {0}'.format(jobs[0][0]))
+    print('Jobs will be written to: {0}'.format(logfolder))
 
 if not args.dry:
     s.run_jobs(log_base, cmds=cmds, add_fp16=True, host2cmd_adds=host2cmd, remap=remap)
